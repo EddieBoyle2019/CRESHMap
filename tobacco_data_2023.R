7ecd66890cb6f66ddfbc2efb8169b07e827f30eb
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-11-14 15:03:39 +0000

    Comments, typos

--- tobacco_data_2023.R
+++ tobacco_data_2023.R
@@ -13,10 +13,11 @@ data_nrs_sap <- read_csv("sape-2021.csv")
 
 ## Transform data
 
+#cleaning, wrangling, matching postcodes to datazones
 data_tobacco %>%
   #remove 'Inactive' records from Status field
   filter(Status=='Active') %>%
-  #rempve duplicates based on three columns identifying retail outlet
+  #remove duplicates based on three columns identifying retail outlet
   distinct(Name, Address, Postcode, .keep_all = TRUE) %>%
   #remove unnecessary fields
   select(-c ('Date of Query', 'Time of Query')) %>%
@@ -27,7 +28,7 @@ data_tobacco %>%
   #aggregate by data zone and add count column (all retail categories)
   add_count(DataZone2011Code, name = "dz_count") -> data_tobacco_transformed
 
-#count types of tobacco outlets by type, in each datazone
+#count tobacco outlets by type, in each datazone
 data_tobacco_transformed %>%
   rename(products_sold = 'Products Sold') %>%
   #add_count(DataZone2011Code, products_sold, name = "dz_count_cats") %>%
@@ -57,5 +58,5 @@ data_tobacco_transformed_densities %>%
   distinct(DataZone2011Code, dz_count, t_and_nv_count, nv_only_count, t_only_count, total_pop, all_density, t_and_nv_density, nv_only_density, t_only_density, .keep_all = FALSE) -> data_tobacco_transformed_final
   #3781 final rows in total, as many of the 6676 datazones have 0 retail outlets
 
-#export as CSV
+## Export data
 write.csv(data_tobacco_transformed_final, "tobacco_data_transformed.csv")
