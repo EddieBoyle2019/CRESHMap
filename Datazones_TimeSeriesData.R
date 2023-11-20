2cdd475b599eaa7119c119336de07ed832b727f2
EddieBoyle2019 <edwardgboyle@hotmail.com>
2023-11-20 13:02:26 +0000

    Removed index numbers from exported file

--- Datazones_TimeSeriesData.R
+++ Datazones_TimeSeriesData.R
@@ -42,6 +42,6 @@ master_data_2024_tobacco %>%
   select(DataZone11_ID, NameDZ11, MidYr2012DZPop, MidYr2016DZPop, MidYr2020DZPop, AR12_All_PopAdjCnt, AR12_On_PopAdjCnt, AR12_Off_PopAdjCnt, TR12_PopAdjCnt, AR16_All_PopAdjCnt,AR16_On_PopAdjCnt, AR16_Off_PopAdjCnt, TR16_PopAdjCnt, AR20_All_PopAdjCnt, AR20_On_PopAdjCnt, AR20_Off_PopAdjCnt, TR20_PopAdjCnt, AR24_All_PopAdjCnt, AR24_On_PopAdjCnt, AR24_Off_PopAdjCnt, TR24_PopAdjCnt, PopDen_2012psqkm, PopDen_2016psqkm, PopDen_2020psqkm) -> master_data_2024
     
 ## Export data
-write.csv(master_data_2024, "Datazones_TimeSeriesData_2024_update.csv")
+write.csv(master_data_2024, "Datazones_TimeSeriesData_2024_update.csv", row.names = FALSE)
   
   
